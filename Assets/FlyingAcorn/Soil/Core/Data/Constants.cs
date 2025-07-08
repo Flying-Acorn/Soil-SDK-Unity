0607c47e3e291600eb80f3f4e3e73c29932b14b7
Amir Hakimnejad <amirhhakimnejad@gmail.com>
2025-07-08 15:53:29 +0330

    Refactor how we choose our base api

--- Assets/FlyingAcorn/Soil/Core/Data/Constants.cs
+++ Assets/FlyingAcorn/Soil/Core/Data/Constants.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace FlyingAcorn.Soil.Core.Data
@@ -6,29 +7,36 @@ namespace FlyingAcorn.Soil.Core.Data
     {
         internal const string DemoAppID = "c425a46d-5a49-4986-b3fe-e9d61cd957d3";
         internal const string DemoAppSDKToken = "8c500e120772a66a1daad9cdfebedbaa3f31d6949ce8d41c94b49f125401ff00";
-
         internal static string ApiUrl
         {
             get
             {
-                if (SoilServices.UserInfo == null)
-                    return FallBackApiUrl;
-                var region = SoilServices.UserInfo.country;
-                return string.IsNullOrEmpty(region)
-                    ? FallBackApiUrl
-                    : APIPerRegion.GetValueOrDefault(region, FallBackApiUrl);
+                return DataUtils.FindApiUrl();
             }
         }
 
         internal const int DefaultTimeout = 6;
         internal const string FallBackApiUrl = "https://wwsoil.flyingacorn.studio/api";
+        internal const string IRApiUrl = "https://soil.flyingacorn.ir/api";
 
-        private static readonly Dictionary<string, string> APIPerRegion = new Dictionary<string, string>
+        internal static readonly List<RegionSettings> APIPerRegion = new()
         {
-            { "IR", "https://irsoil.flyingacorn.studio/api" },
-            { "WW", FallBackApiUrl },
-            { "", FallBackApiUrl },
+            new RegionSettings {Region = Region.WW, ApiUrl = FallBackApiUrl},
+            new RegionSettings {Region = Region.IR, ApiUrl = IRApiUrl}
         };
+
+        [Serializable]
+        public class RegionSettings
+        {
+            public Region Region;
+            public string ApiUrl;
+        }
+
+        public enum Region
+        {
+            WW,
+            IR
+        }
 
         public enum Store
         {
@@ -37,8 +45,8 @@ namespace FlyingAcorn.Soil.Core.Data
             Postman,
             GooglePlay,
             AppStore,
-            CafeBazaar,
-            Myket,
+            CafeBazaar, // Iran Local Store
+            Myket, // Iran Local Store
             Github,
             LandingPage
         }
--- Assets/FlyingAcorn/Soil/Core/Data/DataUtils.cs
+++ Assets/FlyingAcorn/Soil/Core/Data/DataUtils.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using FlyingAcorn.Analytics;
 using UnityEngine;
+using static FlyingAcorn.Soil.Core.Data.Constants;
 
 namespace FlyingAcorn.Soil.Core.Data
 {
@@ -33,17 +34,17 @@ namespace FlyingAcorn.Soil.Core.Data
             return build;
         }
 
-        public static Constants.Store GetStore()
+        public static Store GetStore()
         {
             if (!_buildSettings)
                 _buildSettings = Resources.Load<BuildData.BuildData>("Build_Settings");
-            var storeName = Constants.Store.Unknown;
-            if (_buildSettings && _buildSettings.StoreName != Constants.Store.Unknown)
+            var storeName = Store.Unknown;
+            if (_buildSettings && _buildSettings.StoreName != Store.Unknown)
                 storeName = _buildSettings.StoreName;
-            if (storeName != Constants.Store.Unknown) return storeName;
+            if (storeName != Store.Unknown) return storeName;
             if (!Application.isEditor)
                 MyDebug.LogError("Store name is not set in BuildData");
-            return Constants.Store.Unknown;
+            return Store.Unknown;
         }
 
         public static IEnumerable<FieldInfo> GetAllFields(this Type t)
@@ -55,6 +56,71 @@ namespace FlyingAcorn.Soil.Core.Data
                                        BindingFlags.Static | BindingFlags.Instance |
                                        BindingFlags.DeclaredOnly;
             return t.GetFields(flags).Concat(GetAllFields(t.BaseType));
+        }
+
+        public static RegionSettings GetSettingsForTimeZone()
+        {
+            var tz = System.TimeZoneInfo.Local;
+
+            var utcOffset = tz.BaseUtcOffset;
+            var standardName = tz.StandardName ?? string.Empty;
+
+            var region = Region.WW;
+
+            if (utcOffset == TimeSpan.FromHours(3.5) || utcOffset == TimeSpan.FromHours(4.5) ||
+                standardName.Contains("Iran", StringComparison.OrdinalIgnoreCase) ||
+                standardName.Contains("Tehran", StringComparison.OrdinalIgnoreCase))
+            {
+                region = Region.IR;
+            }
+
+            MyDebug.Verbose($"TimeZone - Offset: {utcOffset}, StandardName: {standardName}, Mapped Region: {region}");
+
+            return APIPerRegion.Find(x => x.Region == region) ?? new RegionSettings
+            {
+                Region = region,
+                ApiUrl = FallBackApiUrl
+            };
+        }
+
+        internal static string FindApiUrl()
+        {
+            switch (GetStore())
+            {
+                case Store.CafeBazaar:
+                case Store.Myket:
+                    return IRApiUrl;
+                case Store.LandingPage:
+                case Store.Unknown:
+                case Store.BetaChannel:
+                case Store.Postman:
+                case Store.GooglePlay:
+                case Store.AppStore:
+                case Store.Github:
+                default:
+                    break;
+            }
+
+            var timezoneSettings = GetSettingsForTimeZone();
+            
+            if (SoilServices.UserInfo?.country == null)
+            {
+                MyDebug.Verbose($"No user info available, using timezone-based URL: {timezoneSettings.ApiUrl}");
+                return timezoneSettings.ApiUrl ?? FallBackApiUrl;
+            }
+            
+            var region = SoilServices.UserInfo.country;
+            var regionEnum = Enum.TryParse(region, true, out Region regionParsed) ? regionParsed : Region.WW;
+            var settingForCountry = APIPerRegion.Find(x => x.Region == regionEnum);
+            
+            if (settingForCountry == null || settingForCountry.Region == Region.WW)
+            {
+                MyDebug.Verbose($"User region is WW or not found ({regionEnum}), preferring timezone-based URL: {timezoneSettings.ApiUrl}");
+                return timezoneSettings.ApiUrl ?? FallBackApiUrl;
+            }
+            
+            MyDebug.Verbose($"Using country-specific API URL for region {regionEnum}: {settingForCountry.ApiUrl}");
+            return settingForCountry.ApiUrl ?? FallBackApiUrl;
         }
     }
 }
